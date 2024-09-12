7bdfd81cb6231267ef97bf6b1d1bdcd7f9aad8ba
KafuiDotse <kafuikwamedotse321@gmail.com>
2024-09-11 22:47:25 -0700

    commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KafuiDotse/Go-Tutorial
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/KafuiDotse/Go-Tutorial/pack"
+)
+
+func main() {
+	fmt.Println(pack.Add(2, 3))
+}
