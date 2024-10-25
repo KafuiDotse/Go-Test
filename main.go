2887acd8b5902ffb294769be453f62c8d7e41dfc
KafuiDotse <kafuikwamedotse321@gmail.com>
2024-10-25 12:37:55 -0700

    f commit

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func main() {
-	fmt.Println("Hello")
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://kafuikwamedotse321:VuIlyNf4JXb6ZvFQ@cluster0.gg6tu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	database, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(database)
+	}
 }
